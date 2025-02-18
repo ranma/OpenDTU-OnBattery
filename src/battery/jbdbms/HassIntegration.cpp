helgeerbe/OpenDTU

restructure battery's HASS integration

src/battery/jbdbms/HassIntegration.cpp
@@ -1,0 +1,48 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <battery/jbdbms/HassIntegration.h>
+
+namespace BatteryNs::JbdBms {
+
+void HassIntegration::publishSensors() const
+{
+    ::BatteryNs::HassIntegration::publishSensors();
+
+    //            caption                  icon                    topic                    dev. class     state class    unit
+    publishSensor("Voltage",               "mdi:battery-charging", "voltage",               "voltage",     "measurement", "V");
+    publishSensor("Current",               "mdi:current-dc",       "current",               "current",     "measurement", "A");
+    publishSensor("Cell Min Voltage",      NULL,                   "CellMinMilliVolt",      "voltage",     "measurement", "mV");
+    publishSensor("Cell Average Voltage",  NULL,                   "CellAvgMilliVolt",      "voltage",     "measurement", "mV");
+    publishSensor("Cell Max Voltage",      NULL,                   "CellMaxMilliVolt",      "voltage",     "measurement", "mV");
+    publishSensor("Cell Voltage Diff",     "mdi:battery-alert",    "CellDiffMilliVolt",     "voltage",     "measurement", "mV");
+    publishSensor("Battery Temperature 1", "mdi:thermometer",      "BatteryTempOneCelsius", "temperature", "measurement", "°C");
+    publishSensor("Battery Temperature 2", "mdi:thermometer",      "BatteryTempTwoCelsius", "temperature", "measurement", "°C");
+    publishSensor("Alarms Bitmask",        NULL,                   "AlarmsBitmask");
+    publishSensor("Charge Cycles",         "mdi:counter",          "BatteryCycles");
+    publishSensor("Available Capacity",    NULL,                   "ActualBatteryCapacityAmpHours");
+
+    publishBinarySensor("Charging enabled",    "mdi:battery-arrow-up",   "BatteryChargeEnabled",    "yes", "no");
+    publishBinarySensor("Discharging enabled", "mdi:battery-arrow-down", "BatteryDischargeEnabled", "yes", "no");
+    publishBinarySensor("Balancing enabled",   "mdi:scale-balance",      "BatteryBalancingEnabled", "yes", "no");
+
+#define PBS(a, b, c) publishBinarySensor("Alarm: " a, "mdi:" b, "alarms/" c, "1", "0")
+    PBS("Cell Overvoltage",             "battery-alert",                 "CellOverVoltage");
+    PBS("Cell Undervoltage",            "battery-alert",                 "CellUnderVoltage");
+    PBS("Pack Overvoltage",             "fuse-alert",                    "PackOverVoltage");
+    PBS("Pack Undervoltage",            "fuse-alert",                    "PackUnderVoltage");
+    PBS("Charging Overtemperature",     "thermometer-alert",             "ChargingOverTemperature");
+    PBS("Charging Undertemperature",    "thermometer-alert",             "ChargingLowTemperature");
+    PBS("Discharging Overtemperature",  "thermometer-alert",             "DischargingOverTemperature");
+    PBS("Discharging Undertemperature", "thermometer-alert",             "DischargingLowTemperature");
+    PBS("Charging Overcurrent",         "fuse-alert",                    "ChargingOverCurrent");
+    PBS("Discharging Overcurrent",      "fuse-alert",                    "DischargeOverCurrent");
+    PBS("Short Circuit",                "fuse-alert",                    "ShortCircuit");
+    PBS("IC Frontend Error",            "battery-alert-variant-outline", "IcFrontEndError");
+    PBS("MOS Software Lock",            "battery-alert-variant-outline", "MosSoftwareLock");
+    PBS("Reserved 1",                   "battery-alert-variant-outline", "Reserved1");
+    PBS("Reserved 2",                   "battery-alert-variant-outline", "Reserved2");
+    PBS("Reserved 3",                   "battery-alert-variant-outline", "Reserved3");
+#undef PBS
+}
+
+} // namespace BatteryNs::JbdBms

src/battery/jkbms/HassIntegration.cpp
@@ -1,0 +1,41 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <battery/jkbms/HassIntegration.h>
+
+namespace BatteryNs::JkBms {
+
+void HassIntegration::publishSensors() const
+{
+    ::BatteryNs::HassIntegration::publishSensors();
+
+    //            caption              icon                    topic                       dev. class     state class    unit
+    publishSensor("Voltage",           "mdi:battery-charging", "BatteryVoltageMilliVolt",  "voltage",     "measurement", "mV");
+    publishSensor("Current",           "mdi:current-dc",       "BatteryCurrentMilliAmps",  "current",     "measurement", "mA");
+    publishSensor("BMS Temperature",   "mdi:thermometer",      "BmsTempCelsius",           "temperature", "measurement", "°C");
+    publishSensor("Cell Voltage Diff", "mdi:battery-alert",    "CellDiffMilliVolt",        "voltage",     "measurement", "mV");
+    publishSensor("Battery Temperature 1", "mdi:thermometer",  "BatteryTempOneCelsius",    "temperature", "measurement", "°C");
+    publishSensor("Battery Temperature 2", "mdi:thermometer",  "BatteryTempTwoCelsius",    "temperature", "measurement", "°C");
+    publishSensor("Charge Cycles",     "mdi:counter",          "BatteryCycles");
+    publishSensor("Cycle Capacity",    "mdi:battery-sync",     "BatteryCycleCapacity");
+
+    publishBinarySensor("Charging Possible",    "mdi:battery-arrow-up",   "status/ChargingActive",    "1", "0");
+    publishBinarySensor("Discharging Possible", "mdi:battery-arrow-down", "status/DischargingActive", "1", "0");
+    publishBinarySensor("Balancing Active",     "mdi:scale-balance",      "status/BalancingActive",   "1", "0");
+
+#define PBS(a, b, c) publishBinarySensor("Alarm: " a, "mdi:" b, "alarms/" c, "1", "0")
+    PBS("Low Capacity",                "battery-alert-variant-outline", "LowCapacity");
+    PBS("BMS Overtemperature",         "thermometer-alert",             "BmsOvertemperature");
+    PBS("Charging Overvoltage",        "fuse-alert",                    "ChargingOvervoltage");
+    PBS("Discharge Undervoltage",      "fuse-alert",                    "DischargeUndervoltage");
+    PBS("Battery Overtemperature",     "thermometer-alert",             "BatteryOvertemperature");
+    PBS("Charging Overcurrent",        "fuse-alert",                    "ChargingOvercurrent");
+    PBS("Discharging Overcurrent",     "fuse-alert",                    "DischargeOvercurrent");
+    PBS("Cell Voltage Difference",     "battery-alert",                 "CellVoltageDifference");
+    PBS("Battery Box Overtemperature", "thermometer-alert",             "BatteryBoxOvertemperature");
+    PBS("Battery Undertemperature",    "thermometer-alert",             "BatteryUndertemperature");
+    PBS("Cell Overvoltage",            "battery-alert",                 "CellOvervoltage");
+    PBS("Cell Undervoltage",           "battery-alert",                 "CellUndervoltage");
+#undef PBS
+}
+
+} // namespace BatteryNs::JkBms

src/battery/pylontech/HassIntegration.cpp
@@ -1,0 +1,47 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <battery/pylontech/HassIntegration.h>
+
+namespace BatteryNs::Pylontech {
+
+void HassIntegration::publishSensors() const
+{
+    ::BatteryNs::HassIntegration::publishSensors();
+
+    publishSensor("Battery voltage", NULL, "voltage", "voltage", "measurement", "V");
+    publishSensor("Battery current", NULL, "current", "current", "measurement", "A");
+    publishSensor("Temperature", NULL, "temperature", "temperature", "measurement", "°C");
+    publishSensor("State of Health (SOH)", "mdi:heart-plus", "stateOfHealth", NULL, "measurement", "%");
+    publishSensor("Charge voltage (BMS)", NULL, "settings/chargeVoltage", "voltage", "measurement", "V");
+    publishSensor("Charge current limit", NULL, "settings/chargeCurrentLimitation", "current", "measurement", "A");
+    publishSensor("Discharge voltage limit", NULL, "settings/dischargeVoltageLimitation", "voltage", "measurement", "V");
+    publishSensor("Discharge current limit", NULL, "settings/dischargeCurrentLimitation", "current", "measurement", "A");
+    publishSensor("Module Count", "mdi:counter", "modulesTotal");
+
+    publishBinarySensor("Alarm Discharge current", "mdi:alert", "alarm/overCurrentDischarge", "1", "0");
+    publishBinarySensor("Warning Discharge current", "mdi:alert-outline", "warning/highCurrentDischarge", "1", "0");
+
+    publishBinarySensor("Alarm Temperature low", "mdi:thermometer-low", "alarm/underTemperature", "1", "0");
+    publishBinarySensor("Warning Temperature low", "mdi:thermometer-low", "warning/lowTemperature", "1", "0");
+
+    publishBinarySensor("Alarm Temperature high", "mdi:thermometer-high", "alarm/overTemperature", "1", "0");
+    publishBinarySensor("Warning Temperature high", "mdi:thermometer-high", "warning/highTemperature", "1", "0");
+
+    publishBinarySensor("Alarm Voltage low", "mdi:alert", "alarm/underVoltage", "1", "0");
+    publishBinarySensor("Warning Voltage low", "mdi:alert-outline", "warning/lowVoltage", "1", "0");
+
+    publishBinarySensor("Alarm Voltage high", "mdi:alert", "alarm/overVoltage", "1", "0");
+    publishBinarySensor("Warning Voltage high", "mdi:alert-outline", "warning/highVoltage", "1", "0");
+
+    publishBinarySensor("Alarm BMS internal", "mdi:alert", "alarm/bmsInternal", "1", "0");
+    publishBinarySensor("Warning BMS internal", "mdi:alert-outline", "warning/bmsInternal", "1", "0");
+
+    publishBinarySensor("Alarm High charge current", "mdi:alert", "alarm/overCurrentCharge", "1", "0");
+    publishBinarySensor("Warning High charge current", "mdi:alert-outline", "warning/highCurrentCharge", "1", "0");
+
+    publishBinarySensor("Charge enabled", "mdi:battery-arrow-up", "charging/chargeEnabled", "1", "0");
+    publishBinarySensor("Discharge enabled", "mdi:battery-arrow-down", "charging/dischargeEnabled", "1", "0");
+    publishBinarySensor("Charge immediately", "mdi:alert", "charging/chargeImmediately", "1", "0");
+}
+
+} // namespace BatteryNs::Pylontech

ranma/OpenDTU-OnBattery

src/battery/jbdbms/HassIntegration.cpp
@@ -1,0 +1,51 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <battery/jbdbms/HassIntegration.h>
+
+namespace Batteries::JbdBms {
+
+HassIntegration::HassIntegration(std::shared_ptr<Stats> spStats)
+    : ::Batteries::HassIntegration(spStats) { }
+
+void HassIntegration::publishSensors() const
+{
+    ::Batteries::HassIntegration::publishSensors();
+
+    //            caption                  icon                    topic                    dev. class     state class    unit
+    publishSensor("Voltage",               "mdi:battery-charging", "voltage",               "voltage",     "measurement", "V");
+    publishSensor("Current",               "mdi:current-dc",       "current",               "current",     "measurement", "A");
+    publishSensor("Cell Min Voltage",      NULL,                   "CellMinMilliVolt",      "voltage",     "measurement", "mV");
+    publishSensor("Cell Average Voltage",  NULL,                   "CellAvgMilliVolt",      "voltage",     "measurement", "mV");
+    publishSensor("Cell Max Voltage",      NULL,                   "CellMaxMilliVolt",      "voltage",     "measurement", "mV");
+    publishSensor("Cell Voltage Diff",     "mdi:battery-alert",    "CellDiffMilliVolt",     "voltage",     "measurement", "mV");
+    publishSensor("Battery Temperature 1", "mdi:thermometer",      "BatteryTempOneCelsius", "temperature", "measurement", "°C");
+    publishSensor("Battery Temperature 2", "mdi:thermometer",      "BatteryTempTwoCelsius", "temperature", "measurement", "°C");
+    publishSensor("Alarms Bitmask",        NULL,                   "AlarmsBitmask");
+    publishSensor("Charge Cycles",         "mdi:counter",          "BatteryCycles");
+    publishSensor("Available Capacity",    NULL,                   "ActualBatteryCapacityAmpHours");
+
+    publishBinarySensor("Charging enabled",    "mdi:battery-arrow-up",   "BatteryChargeEnabled",    "yes", "no");
+    publishBinarySensor("Discharging enabled", "mdi:battery-arrow-down", "BatteryDischargeEnabled", "yes", "no");
+    publishBinarySensor("Balancing enabled",   "mdi:scale-balance",      "BatteryBalancingEnabled", "yes", "no");
+
+#define PBS(a, b, c) publishBinarySensor("Alarm: " a, "mdi:" b, "alarms/" c, "1", "0")
+    PBS("Cell Overvoltage",             "battery-alert",                 "CellOverVoltage");
+    PBS("Cell Undervoltage",            "battery-alert",                 "CellUnderVoltage");
+    PBS("Pack Overvoltage",             "fuse-alert",                    "PackOverVoltage");
+    PBS("Pack Undervoltage",            "fuse-alert",                    "PackUnderVoltage");
+    PBS("Charging Overtemperature",     "thermometer-alert",             "ChargingOverTemperature");
+    PBS("Charging Undertemperature",    "thermometer-alert",             "ChargingLowTemperature");
+    PBS("Discharging Overtemperature",  "thermometer-alert",             "DischargingOverTemperature");
+    PBS("Discharging Undertemperature", "thermometer-alert",             "DischargingLowTemperature");
+    PBS("Charging Overcurrent",         "fuse-alert",                    "ChargingOverCurrent");
+    PBS("Discharging Overcurrent",      "fuse-alert",                    "DischargeOverCurrent");
+    PBS("Short Circuit",                "fuse-alert",                    "ShortCircuit");
+    PBS("IC Frontend Error",            "battery-alert-variant-outline", "IcFrontEndError");
+    PBS("MOS Software Lock",            "battery-alert-variant-outline", "MosSoftwareLock");
+    PBS("Reserved 1",                   "battery-alert-variant-outline", "Reserved1");
+    PBS("Reserved 2",                   "battery-alert-variant-outline", "Reserved2");
+    PBS("Reserved 3",                   "battery-alert-variant-outline", "Reserved3");
+#undef PBS
+}
+
+} // namespace Batteries::JbdBms

src/battery/jkbms/HassIntegration.cpp
@@ -1,0 +1,44 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <battery/jkbms/HassIntegration.h>
+
+namespace Batteries::JkBms {
+
+HassIntegration::HassIntegration(std::shared_ptr<Stats> spStats)
+    : ::Batteries::HassIntegration(spStats) { }
+
+void HassIntegration::publishSensors() const
+{
+    ::Batteries::HassIntegration::publishSensors();
+
+    //            caption              icon                    topic                       dev. class     state class    unit
+    publishSensor("Voltage",           "mdi:battery-charging", "BatteryVoltageMilliVolt",  "voltage",     "measurement", "mV");
+    publishSensor("Current",           "mdi:current-dc",       "BatteryCurrentMilliAmps",  "current",     "measurement", "mA");
+    publishSensor("BMS Temperature",   "mdi:thermometer",      "BmsTempCelsius",           "temperature", "measurement", "°C");
+    publishSensor("Cell Voltage Diff", "mdi:battery-alert",    "CellDiffMilliVolt",        "voltage",     "measurement", "mV");
+    publishSensor("Battery Temperature 1", "mdi:thermometer",  "BatteryTempOneCelsius",    "temperature", "measurement", "°C");
+    publishSensor("Battery Temperature 2", "mdi:thermometer",  "BatteryTempTwoCelsius",    "temperature", "measurement", "°C");
+    publishSensor("Charge Cycles",     "mdi:counter",          "BatteryCycles");
+    publishSensor("Cycle Capacity",    "mdi:battery-sync",     "BatteryCycleCapacity");
+
+    publishBinarySensor("Charging Possible",    "mdi:battery-arrow-up",   "status/ChargingActive",    "1", "0");
+    publishBinarySensor("Discharging Possible", "mdi:battery-arrow-down", "status/DischargingActive", "1", "0");
+    publishBinarySensor("Balancing Active",     "mdi:scale-balance",      "status/BalancingActive",   "1", "0");
+
+#define PBS(a, b, c) publishBinarySensor("Alarm: " a, "mdi:" b, "alarms/" c, "1", "0")
+    PBS("Low Capacity",                "battery-alert-variant-outline", "LowCapacity");
+    PBS("BMS Overtemperature",         "thermometer-alert",             "BmsOvertemperature");
+    PBS("Charging Overvoltage",        "fuse-alert",                    "ChargingOvervoltage");
+    PBS("Discharge Undervoltage",      "fuse-alert",                    "DischargeUndervoltage");
+    PBS("Battery Overtemperature",     "thermometer-alert",             "BatteryOvertemperature");
+    PBS("Charging Overcurrent",        "fuse-alert",                    "ChargingOvercurrent");
+    PBS("Discharging Overcurrent",     "fuse-alert",                    "DischargeOvercurrent");
+    PBS("Cell Voltage Difference",     "battery-alert",                 "CellVoltageDifference");
+    PBS("Battery Box Overtemperature", "thermometer-alert",             "BatteryBoxOvertemperature");
+    PBS("Battery Undertemperature",    "thermometer-alert",             "BatteryUndertemperature");
+    PBS("Cell Overvoltage",            "battery-alert",                 "CellOvervoltage");
+    PBS("Cell Undervoltage",           "battery-alert",                 "CellUndervoltage");
+#undef PBS
+}
+
+} // namespace Batteries::JkBms

src/battery/pylontech/HassIntegration.cpp
@@ -1,0 +1,50 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include <battery/pylontech/HassIntegration.h>
+
+namespace Batteries::Pylontech {
+
+HassIntegration::HassIntegration(std::shared_ptr<Stats> spStats)
+    : ::Batteries::HassIntegration(spStats) { }
+
+void HassIntegration::publishSensors() const
+{
+    ::Batteries::HassIntegration::publishSensors();
+
+    publishSensor("Battery voltage", NULL, "voltage", "voltage", "measurement", "V");
+    publishSensor("Battery current", NULL, "current", "current", "measurement", "A");
+    publishSensor("Temperature", NULL, "temperature", "temperature", "measurement", "°C");
+    publishSensor("State of Health (SOH)", "mdi:heart-plus", "stateOfHealth", NULL, "measurement", "%");
+    publishSensor("Charge voltage (BMS)", NULL, "settings/chargeVoltage", "voltage", "measurement", "V");
+    publishSensor("Charge current limit", NULL, "settings/chargeCurrentLimitation", "current", "measurement", "A");
+    publishSensor("Discharge voltage limit", NULL, "settings/dischargeVoltageLimitation", "voltage", "measurement", "V");
+    publishSensor("Discharge current limit", NULL, "settings/dischargeCurrentLimitation", "current", "measurement", "A");
+    publishSensor("Module Count", "mdi:counter", "modulesTotal");
+
+    publishBinarySensor("Alarm Discharge current", "mdi:alert", "alarm/overCurrentDischarge", "1", "0");
+    publishBinarySensor("Warning Discharge current", "mdi:alert-outline", "warning/highCurrentDischarge", "1", "0");
+
+    publishBinarySensor("Alarm Temperature low", "mdi:thermometer-low", "alarm/underTemperature", "1", "0");
+    publishBinarySensor("Warning Temperature low", "mdi:thermometer-low", "warning/lowTemperature", "1", "0");
+
+    publishBinarySensor("Alarm Temperature high", "mdi:thermometer-high", "alarm/overTemperature", "1", "0");
+    publishBinarySensor("Warning Temperature high", "mdi:thermometer-high", "warning/highTemperature", "1", "0");
+
+    publishBinarySensor("Alarm Voltage low", "mdi:alert", "alarm/underVoltage", "1", "0");
+    publishBinarySensor("Warning Voltage low", "mdi:alert-outline", "warning/lowVoltage", "1", "0");
+
+    publishBinarySensor("Alarm Voltage high", "mdi:alert", "alarm/overVoltage", "1", "0");
+    publishBinarySensor("Warning Voltage high", "mdi:alert-outline", "warning/highVoltage", "1", "0");
+
+    publishBinarySensor("Alarm BMS internal", "mdi:alert", "alarm/bmsInternal", "1", "0");
+    publishBinarySensor("Warning BMS internal", "mdi:alert-outline", "warning/bmsInternal", "1", "0");
+
+    publishBinarySensor("Alarm High charge current", "mdi:alert", "alarm/overCurrentCharge", "1", "0");
+    publishBinarySensor("Warning High charge current", "mdi:alert-outline", "warning/highCurrentCharge", "1", "0");
+
+    publishBinarySensor("Charge enabled", "mdi:battery-arrow-up", "charging/chargeEnabled", "1", "0");
+    publishBinarySensor("Discharge enabled", "mdi:battery-arrow-down", "charging/dischargeEnabled", "1", "0");
+    publishBinarySensor("Charge immediately", "mdi:alert", "charging/chargeImmediately", "1", "0");
+}
+
+} // namespace Batteries::Pylontech